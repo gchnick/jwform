561a1a7c900451ace1ecc2ff78c62a2441d39cbe
Nick Galan <gnick.alan@gmail.com>
2023-06-09 13:54:55 -0400

    feat: :sparkles: add integrity test and NoIntegrityError

--- src/form/integrity.ts
+++ src/form/integrity.ts
@@ -1,3 +1,15 @@
-export const integrity = (path: string, md5: string): void => {
-    // TODO: Comprobar integridad
+import { NoIntegrityFormError } from "./errors";
+
+const fs = require('fs');
+const md5sum = require('md5');
+
+
+export const integrity = (filePath: string, md5: string): void => {
+    fs.readFile(filePath, (_err: any, buf: any) => {
+        const _md5sum = md5sum(buf);
+
+        if (!((_md5sum as string) === md5)) {
+            throw new NoIntegrityFormError("Form invalid.");
+        }
+    });
 }
