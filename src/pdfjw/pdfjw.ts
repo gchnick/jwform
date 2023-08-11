94be0ee39482d35799007c66303e168e90e2cdb5
Nick Galan <gnick.alan@gmail.com>
2023-08-10 23:13:23 -0400

    refactor(pdfjw): :recycle: change to functions implement

--- src/pdfjw/pdfjw.ts
+++ src/pdfjw/pdfjw.ts
@@ -1,9 +1,17 @@
-import { Data } from '../form/data'
-import { Form } from '../form/form'
+import { TransactionForm } from '../modules/store/accounting/domain/transaction-form'
+import { TransactionRecord } from '../modules/store/accounting/domain/transaction-record'
+import getStore from '../modules/store/store'
+import { TerritoryRegistry } from '../modules/store/territory/territory-registry'
+import { TerritoryRegistryForm } from '../modules/store/territory/territory-registry-form'
 
-export class PdfJW {
+export function fillTransactionRecord(transactionRecord: TransactionRecord, path: string) {
+    const store = getStore({ path }).getValue()
+    const form = new TransactionForm(store, transactionRecord)
+    return form.fillForm()
+}
 
-    generate (form: Form, data: Data): Promise<string> {
-        return form.generatePDF(data)
-    }
+export function fillTerritoryRegistry(territoyRegistry: TerritoryRegistry, path: string) {
+    const store = getStore({ path }).getValue()
+    const form = new TerritoryRegistryForm(store, territoyRegistry)
+    return form.fillForm()
 }
