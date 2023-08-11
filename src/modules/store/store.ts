b1e4114d923f3b01c13a19214e178d4b860903ae
Nick Galan <gnick.alan@gmail.com>
2023-08-10 23:08:13 -0400

    refactor: :recycle: remove undefined value

---
+++ src/modules/store/store.ts
@@ -0,0 +1,31 @@
+
+export type Store = {path: string};
+
+/**
+ * The only instance of our Singleton
+ */
+let instance: ReturnType<typeof makeSingleton<Store>>
+
+const makeSingleton = <T>(initial: T) => {
+    /** Closure of the singleton's value to keep it private */
+    let _value: T = initial
+    /** Only the accessors are returned */
+    return {
+        getValue: (): T => _value,
+        setValue: (value: T) => _value = value,
+    }
+}
+
+const getStore = (initial: Store) => {
+    if (!instance) {
+        instance = makeSingleton<Store>(initial)
+        return instance
+    }
+    if (initial) {
+        throw Error ('Store already initialised')
+    }
+    return instance
+}
+
+export default getStore
+
